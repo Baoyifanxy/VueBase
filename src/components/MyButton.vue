51f837d1c016828a5d7c644dd2c508ab88a7866b
baobao <2631958043@qq.com>
2021-08-02 09:43:41 +0800

    'first'

---
+++ src/components/MyButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>我爱你赵丽颖</h2>
+  <button @click="count++">爱了{{count}}次</button>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+  data() {
+    return {
+      count:0,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h2 {
+    color: aquamarine;
+  }
+</style>
